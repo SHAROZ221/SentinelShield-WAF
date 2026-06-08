17b902024456490bafe3fecf1460c8290d0ed59d
Sharoz <dontknowhowtocode10@gmail.com>
2026-06-08 16:21:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ from flask_cors import CORS
 import json
 import os
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
 # Import our custom modules
 from rules   import inspect_request
 from monitor import record_request, is_blocked, get_stats as monitor_stats
