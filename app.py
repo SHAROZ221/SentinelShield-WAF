eda36071dc09cfdc46d2ba6b99114ed0eba20a17
Sharoz <dontknowhowtocode10@gmail.com>
2026-06-08 16:23:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ import json
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
 
 # Import our custom modules
